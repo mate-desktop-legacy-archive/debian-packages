785030bf669178a6d4b910edaa8ac356005f5bf2
Stefano Karapetsas <stefano@karapetsas.com>
2014-01-29 00:28:42 +0100

    Update build.py

--- build.py
+++ build.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-import sys
-import os
 import codecs
+import datetime
+import os
+import subprocess
+import sys
 
 package = sys.argv[1]
 if package == "":
@@ -22,19 +24,36 @@ os.system("rm -rf debian/")
 os.system("cp -R ~/mate/debian-packages/%s/debian/ ." % package)
 os.system("echo '3.0 (native)' > debian/source/format")
 
+# debian/changelog
+date = datetime.date.today().strftime("%Y%m%d")
+head = subprocess.check_output(["git", "rev-parse", "HEAD"])[:7]
+suffix = "+git." + date + "." + head
+orig_changelog_file = codecs.open("debian/changelog", encoding="utf-8", mode="r")
+orig_changelog = orig_changelog_file.read().splitlines()
+orig_changelog_file.close()
+new_changelog = orig_changelog
+new_changelog[0] = new_changelog[0].replace(")", suffix + ")")
+new_changelog_file = codecs.open("debian/changelog", encoding="utf-8", mode="w")
+new_changelog_file.write("\n".join(new_changelog))
+new_changelog_file.close()
+
+# debian/rules
 orig_rules_file = codecs.open("debian/rules", encoding="utf-8", mode="r")
 orig_rules = orig_rules_file.read()
 orig_rules_file.close()
-new_rules = orig_rules.replace("dh_auto_configure --", "NOCONFIGURE=1 ./autogen.sh --with-gtk=3.0\n\tdh_auto_configure --")
+new_rules = orig_rules
+new_rules = new_rules.replace("dh_auto_configure --", "NOCONFIGURE=1 ./autogen.sh\n\tdh_auto_configure --")
 new_rules = new_rules.replace("#DEB_CONFIGURE_SCRIPT := ./autogen.sh", "DEB_CONFIGURE_SCRIPT := ./autogen.sh")
 new_rules_file = codecs.open("debian/rules", encoding="utf-8", mode="w")
 new_rules_file.write(new_rules)
 new_rules_file.close()
 
 os.system("dpkg-buildpackage -b -Zxz -uc -tc 2>&1 | tee build.log")
-os.system("mv -v build.log ~/mate/build.log")
+os.system("mv -v build.log ~/mate/logs/%s.log" % package)
 os.system("rm -v ../*.changes")
 os.system("mv -v ../*.deb ~/mate/")
-os.chdir("/home/stefano/mate")
+os.chdir(os.path.join(os.path.expanduser("~"), "mate"))
 os.system("rm -rf /tmp/%s/ " % package)
+print "---------------------------------------------------------------"
 os.system("ls -1 *.deb")
+print "---------------------------------------------------------------"
