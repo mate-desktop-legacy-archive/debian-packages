f651c51b9cf664b369d815f752c40b6a2ef1f973
monsta <monsta@inbox.ru>
2019-03-20 15:57:17 +0300

    download.py: fix mate-themes downloading
    themes are in a separate subdir on the server

--- download.py
+++ download.py
@@ -12,7 +12,10 @@ for path in commands.getoutput("find . -name changelog | grep -v mate-desktop-en
 		version = version.split("-")[0]	
 	version_components = version.split(".")
 	major = "%s.%s" % (version_components[0], version_components[1])
-	url = "https://pub.mate-desktop.org/releases/%s/%s-%s.tar.xz" % (major, project, version)
+	if project == "mate-themes":
+		url = "https://pub.mate-desktop.org/releases/themes/%s/%s-%s.tar.xz" % (major, project, version)
+	else:
+		url = "https://pub.mate-desktop.org/releases/%s/%s-%s.tar.xz" % (major, project, version)
 	dest = "%s_%s.orig.tar.xz" % (project, version)
 	command = "wget -c %s -O %s" % (url, dest)
 	print "Running %s" % command
